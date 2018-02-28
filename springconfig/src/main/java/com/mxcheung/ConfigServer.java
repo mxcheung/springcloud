d1c0070716264bbc2bb78b79f0a562f450bcbdf1
max cheung <maxcheung@optusnet.com.au>
2018-02-28 21:34:39 +1100

    Spring config

--- springconfig/src/main/java/com/mxcheung/ConfigServer.java
+++ springconfig/src/main/java/com/mxcheung/ConfigServer.java
@@ -2,9 +2,10 @@ package com.mxcheung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-//@EnableConfigServer
+@EnableConfigServer
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
