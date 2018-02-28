ddb40beecb66095725560d88cd08f21cb1fa8855
max cheung <maxcheung@optusnet.com.au>
2018-02-28 22:43:04 +1100

    Update spring boot tomcat

--- springconfigclient/src/main/java/com/mxcheung/ConfigClient.java
+++ springconfigclient/src/main/java/com/mxcheung/ConfigClient.java
@@ -3,6 +3,8 @@ package com.mxcheung;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ConfigClient {
+public class ConfigClient extends SpringBootServletInitializer {
 
 	@Value("${user.role}")
 	private String role;
@@ -23,5 +25,10 @@ public class ConfigClient {
 	@RequestMapping(value = "/whoami/{username}", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
 	public String whoami(@PathVariable("username") String username) {
 		return String.format("Hello!  You're %s and you'll become a(n) %s...\n", username, role);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ConfigClient.class);
 	}
 }
--- springconfigserver/src/main/java/com/mxcheung/ConfigServer.java
+++ springconfigserver/src/main/java/com/mxcheung/ConfigServer.java
@@ -2,12 +2,19 @@ package com.mxcheung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServer {
+public class ConfigServer extends SpringBootServletInitializer  {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ConfigServer.class);
     }
 }
